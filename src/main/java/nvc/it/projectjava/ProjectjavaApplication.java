089ec2615ea2bf1691e728b711942b8956a8dda4
tanakorn06 <boonsukurtbew@gmail.com>
2022-02-04 17:31:46 +0700

    first commit

---
+++ src/main/java/nvc/it/projectjava/ProjectjavaApplication.java
@@ -0,0 +1,13 @@
+package nvc.it.projectjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectjavaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/nvc/it/projectjava/service/ProductService.java
@@ -0,0 +1,29 @@
+package nvc.it.projectjava.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import nvc.it.projectjava.model.Product;
+import nvc.it.projectjava.repository.ProductRepository;
+
+@Service
+public class ProductService {
+    
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getProducts(){
+        return productRepository.findAll();
+    }
+        
+
+    public Optional<Product> findById(String id){
+        return productRepository.findById(id);
+    }
+    public List<Product> findByProductName(String name){
+        return productRepository.findByNameContaining(name);
+    }
+}
